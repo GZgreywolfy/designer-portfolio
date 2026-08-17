032745307f2f987f18d05771ebed5b06b10797ee
Greywolfy <160043884+GZgreywolfy@users.noreply.github.com>
2026-08-17 21:34:59 +0800

    refactor: avatar import 改为 .webp

--- src/data/content.js
+++ src/data/content.js
@@ -1,5 +1,5 @@
 import heroPoster from '../assets/images/hero-poster.png'
-import avatar from '../assets/images/avatar.png'
+import avatar from '../assets/images/avatar.webp'
 import p1 from '../assets/images/p1.png'
 import p2 from '../assets/images/p2.png'
 import p3 from '../assets/images/p3.png'
